354c1097b99ef24c8b307fd286d231ab5b48b659
Dilmurd <0nishonov.dilmurod@gmail.com>
2024-09-13 05:56:10 +0500

    ozgartirdim

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const time = document.querySelector(".time")
+console.log(time);
+function currentTime(){
+    let date = new Date()
+    let hour = date.getHours().toString().padStart(2, "0")
+    let minute = date.getMinutes().toString().padStart(2, "0")
+    let second = date.getSeconds().toString().padStart(2, "0")
+    time.innerHTML = `${hour}:${minute}:${second}`
+}
+currentTime()
+setInterval (()=>{
+    currentTime()
+}, 1000)
+
+
+
+
+
 // console.log("start");
 // // 1-masala
 // function getCount(str) {
